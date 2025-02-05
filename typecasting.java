b26c02500da9194e39649563c6c879f07d0c2732
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-05 21:47:22 +0545

    type casting bla bla bla

---
+++ typecasting.java
@@ -0,0 +1,14 @@
+public class typecasting {
+
+    public static void main(String[] args){
+
+        int a= 10;
+        double b = a;// automatic casting
+        System.out.println(a+"\n"+b);
+
+        double c = 9.78d;
+        int d = (int) c; // manual casting
+        System.out.println(c+"\n"+d);
+
+    }
+}
