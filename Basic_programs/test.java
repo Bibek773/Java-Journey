4ea399cdc1ef87d06eb767f50610630bba4fdd35
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-05 22:47:24 +0545

    ..

---
+++ Basic_programs/test.java
@@ -0,0 +1,20 @@
+// java program file must be save as the same name as class
+public class test {
+  public static void main(String[] args) {
+    //System.out.println("Hello World");
+    String name= "bibek";
+    System.out.println(name);
+    int number=14;
+    System.out.println(number);
+    boolean b= true;
+    float a= 3.56f;
+    char c='a';
+    System.out.println(b);
+    System.out.println(a);
+    System.out.println(c);
+    System.out.println("hello "+name);
+    String lastname= "ghimire";
+    String fullname = name + lastname;
+    System.out.println(fullname);
+  }
+}
