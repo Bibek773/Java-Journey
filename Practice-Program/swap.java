4056936adf14fabf354eab4cd6ac9eb023ede65a
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-06 19:44:09 +0545

    swapping of program

---
+++ Practice-Program/swap.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class swap {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+//        System.out.println("Enter first name");
+//        String fname = input.nextLine();
+//        System.out.println("Enter second name");
+//        String lname = input.nextLine();
+//        System.out.println("First name = " + fname+ " Last name= " + lname);
+//        String temp = fname;
+//        fname = lname;
+//        lname = temp;
+//        System.out.println("First name = " + fname+ " Last name= " + lname);
+    }
+}
