18643f21f7c1e91ae9c2ad97f241d9322cde8308
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-08 20:40:39 +0545

    array in java

--- Basic_programs/array.java
+++ Basic_programs/array.java
@@ -9,6 +9,22 @@ public class array
 //        cars[1]="bmw";
 //        System.out.println(cars[0]);
 //        System.out.println(cars[1]);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of students");
+        int n = input.nextInt();
+        input.nextLine();
+        String[] student= new String[n];
+        for(int i=0; i<n; i++){
+            System.out.println("Enter the "+ (i+1) +"th student name");
+            student[i]=input.nextLine();
+
+        }
+        System.out.println("Name of student are as follow");
+        for(int i=0; i<n; i++){
+            System.out.println((i+1)+". "+student[i]);
+
+
+        }
 
     }
 
