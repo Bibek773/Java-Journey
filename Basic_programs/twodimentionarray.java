6c28c531c42892bb9dcf17f1f1faa94fe2efef5a
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-08 20:55:31 +0545

    input matrix and display

--- Basic_programs/twodimentionarray.java
+++ Basic_programs/twodimentionarray.java
@@ -5,15 +5,37 @@ public class twodimentionarray
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        System.out.println("enter the dimention of matrix");
+        System.out.println("enter the dimension of matrix");
         System.out.println("Enter number of rows");
         int row= input.nextInt();
         System.out.println("Enter number of columns");
         int column= input.nextInt();
 
         int[][] matrix= new int[row][column];
-        for(int i= 0; i<row; )
-        System.out.println("Enter t");
+
+        //taking input
+        for(int i= 0; i<row; i++) {
+
+            for(int j= 0; j<column; j++) {
+
+                System.out.println("Enter the data for A"+(i+1)+","+(j+1));
+                matrix[i][j]=input.nextInt();
+
+            }
+
+        }
+        //displaying
+        System.out.println("The matrix is");
+        for(int i= 0; i<row; i++) {
+
+            for(int j= 0; j<column; j++) {
+
+                System.out.print(matrix[i][j]+"\t");
+
+
+            }
+            System.out.println();
+        }
 
 
 
