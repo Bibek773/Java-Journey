d05f9d6cf9da70710e76391949091f1dbd55e3ba
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-08 20:33:28 +0545

    array in java

---
+++ Basic_programs/array.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class array
+{
+
+    public static void main(String[] args){
+
+//        String[] cars= new String[2];
+//        cars[0]="bugatti";
+//        cars[1]="bmw";
+//        System.out.println(cars[0]);
+//        System.out.println(cars[1]);
+
+    }
+
+}
