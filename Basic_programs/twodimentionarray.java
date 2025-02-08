5e2d8a69150c0a92e8afac6384ed0e7f0369db57
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-08 20:49:12 +0545

    file created

---
+++ Basic_programs/twodimentionarray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class twodimentionarray
+{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the dimention of matrix");
+        System.out.println("Enter number of rows");
+        int row= input.nextInt();
+        System.out.println("Enter number of columns");
+        int column= input.nextInt();
+
+        int[][] matrix= new int[row][column];
+        for(int i= 0; i<row; )
+        System.out.println("Enter t");
+
+
+
+    }
+
+}
