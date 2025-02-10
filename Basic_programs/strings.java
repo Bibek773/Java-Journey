780728bf7fee3007028b0c410e427dc14835cb76
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-10 22:24:51 +0545

    string methods

---
+++ Basic_programs/strings.java
@@ -0,0 +1,21 @@
+package Basic_programs;
+
+public class strings {
+    public static void main(String[] args) {
+
+        String name = "Bro";
+        boolean result = name.equals("Bro");// gives true
+        System.out.println(result);
+
+        boolean result2 = name.equalsIgnoreCase("bro");// gives true
+        System.out.println(result2);
+
+        int len = name.length();
+        System.out.println(len);
+
+//        char abc = name.charAt(1);
+//        int abc = name.indexOf("r");
+        
+        System.out.println(abc);
+    }
+}
