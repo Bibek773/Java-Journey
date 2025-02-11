b1f39ab596aa806600fbbcbd5149314ffbc93163
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-11 17:05:57 +0545

    wrapper class basics

---
+++ Basic_programs/wrapperclass.java
@@ -0,0 +1,28 @@
+public class wrapperclass {
+
+    public static void main(String[] args){
+
+        //wrapper class provides a way to use primitive data types as objects
+
+        /*
+        primitive  | Wrapper
+        -----------|------------
+        boolean    | Boolean
+        char       | Character
+        int        | Integers
+        double     | Double
+         */
+        // autoboxing = the automatic conversion that Java compiler makes between the primitive types and their corresponding  wrapper classes
+        //unboxing = the reverse of autoboxing. Automatic conversion of wrapper class is primitive
+//        ArrayList<int> myNumbers = new ArrayList<int>(); // Invalid
+//        ArrayList<Integer> myNumbers = new ArrayList<Integer>(); // Vlid
+        Boolean a = true;
+        Character b= '@';
+        Integer c =123;
+        Double d = 3.23;
+        String e = "bibek";
+
+        
+
+    }
+}
