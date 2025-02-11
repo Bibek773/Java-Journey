6526ffe63ec36d58fddb555b41574e1336bb2455
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-11 22:17:52 +0545

    2d arraylist

--- Basic_programs/arraylist.java
+++ Basic_programs/arraylist.java
@@ -9,13 +9,43 @@ public class arraylist {
         elements can be added and removed after compilation phase
         store reference data type
          */
-        ArrayList<String> food = new ArrayList<String>();
-        food.add("pizza");//add new element to array list
-        food.add("burger");
-        food.add("mo:mo");
+//        ArrayList<String> food = new ArrayList<String>();
+//        food.add("pizza");//add new element to array list
+//        food.add("burger");
+//        food.add("mo:mo");
+//
+//        food.set(0, "chowmin");//set chowmin to index 0
+//        food.remove(2);// remove item of index 2
+//        food.clear(); // clear the list
+//
+//
+//        for(int i=0; i<food.size(); i++){
+//            System.out.println(food.get(i));
+//        }
+        // --------------------------------------
+        // 2 dimension array list
 
-        for(int i=0; i<food.size(); i++){
-            System.out.println(food.get(i));
-        }
+            ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
+
+            ArrayList<String> bakeryList = new ArrayList<>();
+            bakeryList.add("pasta");
+            bakeryList.add("garlic bread");
+            bakeryList.add("donuts");
+
+            ArrayList<String> produceList = new ArrayList<>();
+            produceList.add("tomato");
+            produceList.add("potato");
+            produceList.add("gingers");
+
+            ArrayList<String> drinkList = new ArrayList<>();
+            drinkList.add("pepsi");
+            drinkList.add("coffee");
+
+            groceryList.add(bakeryList);
+            groceryList.add(produceList);
+            groceryList.add(drinkList);
+
+            System.out.println(groceryList);
+
     }
 }
